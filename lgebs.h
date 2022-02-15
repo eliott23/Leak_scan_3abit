99b89389d28ccb3aab9eb6f0be9a1a45915cb4da
Abderrahmane Ababachir <aababach@e1r11p6.1337.ma>
2022-02-15 22:28:40 +0100

    zeb

--- lgebs.h
+++ lgebs.h
@@ -3,3 +3,4 @@
 void     *lgebs(void *ptr, char *file, int line, int option, size_t size);
 #define malloc(X)	lgebs(0, __FILE__, __LINE__, 1, X)
 #define free(X)		lgebs(X, __FILE__, __LINE__, 2, 0)
+#endif
